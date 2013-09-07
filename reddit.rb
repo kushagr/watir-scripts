2a7288434288367f6e7db69a771571ca90b3bfca
Kushagra <kushagr@kushagra.desktop>
2013-09-07 16:56:35 +0530

    reddit.rb handles pages with no links

--- reddit.rb
+++ reddit.rb
@@ -4,8 +4,18 @@ browser = Watir::Browser.new :firefox
 browser.goto "http://reddit.com"
 5.times do 
 	visible_links = browser.links.find_all(&:present?) 
+	
+	#check if page has visible links
     link_count = visible_links.count
+    if link_count == 0
+    	browser.back
+    	visible_links = browser.links.find_all(&:present?)
+    end
+    
+    #click random link	
     random_link = visible_links.sample
     random_link.click
 end	
+
+
 browser.close
